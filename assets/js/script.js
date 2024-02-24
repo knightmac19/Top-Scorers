6cca7b6dc64e603b8ab4bf01289f2ed54c3f0d45
Patrick Dunn <pmdunn78@gmail.com>
2024-02-23 21:44:20 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const initialCall = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// initialCall(url, options);
